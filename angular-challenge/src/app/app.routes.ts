c9bbfde0f2d1849dbbcbd6e2b39400df399b49aa
Urvashi <kat.urvashi@gmail.com>
2025-08-17 17:24:35 +0200

    Add route for Aircraft data

--- angular-challenge/src/app/app.routes.ts
+++ angular-challenge/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AircraftDataComponent } from './core/aircraft-data/aircraft-data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: AircraftDataComponent }];
